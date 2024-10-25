3619672b2c2293bf18e1a32f054cf01d153f43c3
Taha Koroglu <m.tahakoroglu@gmail.com>
2024-10-25 09:39:48 +0300

    simple_list.py is updated

--- Python/hafta-4-5/kod/simple_list.py
+++ Python/hafta-4-5/kod/simple_list.py
@@ -1,7 +1,14 @@
 names = ["Önder", "Furkan", "Sefa", "Safa", "Patrick", "Mahmoud", "Cindy"]
 print(f"names = {names}")
 print(f"type(names) = {type(names)}")
-print(f"type(names[0]) = {type(names[0])}")
+print(f"names[0] = {names[0]}")
+print(f"names[1] = {names[1]}")
+print(f"names[2] = {names[2]}")
+print(f"names[5] = {names[5]}")
+print(f"names[6] = {names[6]}")
+print(f"names[-1] = {names[-1]}")
+print(f"names[-2] = {names[-2]}")
+print(f"type(names[1]) = {type(names[1])}")
 print(f"Sınıfta {len(names)} öğrenci var.")
 # listedeki isimleri tek tek ekrana yazdıralım
 for name in names:
