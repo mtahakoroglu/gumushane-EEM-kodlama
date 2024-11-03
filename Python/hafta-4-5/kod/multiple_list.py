b898bf1f183cef71a82cf955b45719644adaa369
Taha Koroglu <m.tahakoroglu@gmail.com>
2024-11-03 17:17:42 +0300

    Python 6. ve 7. hafta klasörü oluşturuldu

--- Python/hafta-4-5/kod/multiple_list.py
+++ Python/hafta-4-5/kod/multiple_list.py
@@ -2,9 +2,9 @@ names = ["Önder", "Furkan", "Sefa", "Safa", "Patrick", "Mahmoud", "Cindy"]
 ages = [20, 21, 21, 20, 23, 25, 24]
 weight = [73.6, 75.2, 64.1, 67.9, 71.1, 68.1, 50.3]
 attendance = [True, True, True, True, False, True, True]
-# for döngüsü ile listedeki öğrencilerin isimlerini, yaşlarını ve kilolarını ekrana yazdıralım
+# for döngüsü ile listedeki öğrencilerin isimlerini, yaşlarını, kilolarını ve devamsızlık durumlarını ekrana yazdıralım
 for i in range(len(names)):
-    if attendance[i]: # eğer öğrenci mevcut ise bilgileri yazdır
+    if attendance[i]:
         print(f"{names[i]} {ages[i]} yaşında olup {weight[i]} kg ağırlığındadır.")
-    else: # eğer öğrenci yok ise o zaman bilgileri yazdırma
+    else:
         print(f"{names[i]} bugün sınıfta yoktur.")
