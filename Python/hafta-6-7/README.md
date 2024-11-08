1002b67867710f4dc816937480cd0a496b47b5b6
Taha Koroglu <m.tahakoroglu@gmail.com>
2024-11-08 10:15:05 +0300

    6. ve 7. hafta materyallerine ara sınav örnek soru kodu eklendi.

--- Python/hafta-6-7/README.md
+++ Python/hafta-6-7/README.md
@@ -10,7 +10,7 @@ mixed_list = ["Sefa", 21, 64.1, True]
 print(f"mixed_list = {mixed_list}")
 mixed_list.append(["Patrick", 22, 67.3, False])
 print(f"mixed_list = {mixed_list}")
-# listenin son elemanı olan listenin ilk elemanına erişelim
+# karışık listenin sonunan eklenen listenin ilk elemanına erişelim
 print(f"mixed_list[-1][0] = {mixed_list[-1][0]}")
 ```
 
--- Python/hafta-6-7/kod/mixed_list.py
+++ Python/hafta-6-7/kod/mixed_list.py
@@ -2,5 +2,5 @@ mixed_list = ["Sefa", 21, 64.1, True]
 print(f"mixed_list = {mixed_list}")
 mixed_list.append(["Patrick", 22, 67.3, False])
 print(f"mixed_list = {mixed_list}")
-# listenin son elemanı olan başka bir listenin ilk elemanına erişelim
+# listenin son elemanı olan eklenen listenin ilk elemanına erişelim
 print(f"mixed_list[-1][0] = {mixed_list[-1][0]}")
