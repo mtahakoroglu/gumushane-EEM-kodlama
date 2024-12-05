f3324ac1a34e2380aecd1026fba003bbf3e0667b
mtahakoroglu <m.tahakoroglu@gmail.com>
2024-12-06 02:46:44 +0300

    Hafta 10-11 materyalleri eklendi.

--- Python/hafta-10-11/kod/popped_elements_new_list.py
+++ Python/hafta-10-11/kod/popped_elements_new_list.py
@@ -2,3 +2,14 @@ names = ["Ali", "berat", "Cindy", "davud", "SAFA",
          "Ece", "sefa", "Gizem", "Selin", "İsmail"]
 print(f"names = {names}")
 print(f"names isimli listede {len(names)} kişi vardır.")
+# listeden 's' ile başlayan isimleri silelim
+popped_elements = []
+i = 0
+# while döngüsü ile yapalım
+while i < len(names):
+    if names[i].lower()[0] == "s":
+        popped_elements.append(names.pop(i))
+    else:
+        i += 1
+print(f"Listenin son hâli {names}")
+print(f"Silinen isimler {popped_elements}")
